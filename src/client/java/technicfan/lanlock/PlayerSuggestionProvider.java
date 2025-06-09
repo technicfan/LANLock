dcc0764ff821322c58611499ebfb693684eadad7
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-06-09 02:01:54 +0200

    fix crash

--- src/client/java/technicfan/lanlock/PlayerSuggestionProvider.java
+++ src/client/java/technicfan/lanlock/PlayerSuggestionProvider.java
@@ -6,19 +6,21 @@ import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class PlayerSuggestionProvider implements SuggestionProvider<FabricClientCommandSource> {
     @Override
     public CompletableFuture<Suggestions> getSuggestions(CommandContext<FabricClientCommandSource> context, SuggestionsBuilder builder) {
-        assert MinecraftClient.getInstance().getServer() != null;
-        Collection<String> playerNames = List.of(MinecraftClient.getInstance().getServer().getPlayerNames());
-
-        for (String playerName : playerNames) {
-            builder.suggest(playerName);
+        if (MinecraftClient.getInstance().getServer() != null) {
+            for (String playerName : MinecraftClient.getInstance().getServer().getPlayerNames()) {
+                builder.suggest(playerName);
+            }
+        } else if (MinecraftClient.getInstance().world != null) {
+            for (AbstractClientPlayerEntity player : MinecraftClient.getInstance().world.getPlayers()) {
+                builder.suggest(player.getName().getString());
+            }
         }
 
         return builder.buildFuture();
